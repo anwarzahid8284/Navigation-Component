037c2c8c39b059456fe358673d39be7091c29584
AnwarZahid <anwarzahid.bcs@gmail.com>
2021-03-31 23:23:28 +0500

    navigation component

--- app/src/main/java/com/example/navigationcomponent/SecondFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/SecondFragment.kt
@@ -1,7 +1,20 @@
 package com.example.navigationcomponent
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class SecondFragment : Fragment(R.layout.fragment_second) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginBtnID.setOnClickListener{
+            var userName:String=secondFragment_userNameID.text.toString().trim()
+            var userPassword:String=secondFragment_userPasswordID.text.toString().trim()
+            val action=SecondFragmentDirections.actionSecondFragmentToWelcomeFragment(userName,userPassword)
+            findNavController().navigate(action)
+        }
+    }
 
 }
---
+++ app/src/main/java/com/example/navigationcomponent/WelcomeFragment.kt
@@ -0,0 +1,21 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_welcome.*
+
+class WelcomeFragment:Fragment(R.layout.fragment_welcome) {
+    private val args:WelcomeFragmentArgs by navArgs()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        welcomeFragment_OkBtnID.setOnClickListener{
+            welcomeFragment_GetUserNameID.text=args.userName
+            welcomeFragment_GetPasswordID.text=args.userPassword
+            val action=WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }
+    }
+}
